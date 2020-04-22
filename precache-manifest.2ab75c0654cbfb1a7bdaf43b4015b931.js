1776ae6ffd67170a72736dfffbef6e25400ff1f0
Le Minh Triet <leminhtriet@Les-MacBook-Pro.local>
2020-04-22 14:13:05 +0700

    Updates

---
+++ precache-manifest.2ab75c0654cbfb1a7bdaf43b4015b931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28a110066bc967ae9e292e1684d08e9",
+    "url": "/calculator_react/index.html"
+  },
+  {
+    "revision": "a929ccde56baa2b5a8d4",
+    "url": "/calculator_react/static/css/main.4feabb78.chunk.css"
+  },
+  {
+    "revision": "6bd721228f6f72a9c242",
+    "url": "/calculator_react/static/js/2.eddd6347.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator_react/static/js/2.eddd6347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a929ccde56baa2b5a8d4",
+    "url": "/calculator_react/static/js/main.6b06129f.chunk.js"
+  },
+  {
+    "revision": "d13eec3caf8d8525dc70",
+    "url": "/calculator_react/static/js/runtime-main.81823212.js"
+  }
+]);
